a89895d8190e5ea2e44e9d84d3e481426a3c15cf
KevinB6766 <tuu21653@temple.edu>
2025-03-26 16:14:51 -0400

    Finished

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -3,9 +3,6 @@ package edu.temple.dicethrow
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
-import kotlin.text.replace
-
 
 /*
 Our DieThrow application has been refactored to move the dieRoll() logic
@@ -17,63 +14,45 @@ Follow the requirements below to have this app function
 in both portrait and landscape configurations.
 The Activity layout files for both Portrait and Landscape are already provided
 */
-
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
-
-    private var isLandscape = false // Flag to track the orientation
-    private lateinit var buttonFragment: ButtonFragment // instantiate fragment
-    private lateinit var dieFragment: DieFragment // instantiate fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* TODO 1: Load fragment(s)
-            - Show _only_ ButtonFragment if portrait
-            - show _both_ fragments if Landscape
-          */
-        buttonFragment = ButtonFragment()
-        dieFragment = DieFragment()
+        val fragment1 = ButtonFragment()
+        val fragment2 = DieFragment()
 
-        isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-        loadFragmentBasedOnOrientation()   // Check the orientation and load the appropriate fragment
 
-        /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
-        */
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container1, ButtonFragment())
+                    .commit()
+            }
+        } else {
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container1, fragment1)
+                    .replace(R.id.container2, fragment2)
+                    .commit()
+            }
+            else {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container1, fragment1)
+                    .commit()
+            }
+        }
 
-        // This callback function gets invoked when the child Fragment invokes it
-        // Remember to place Fragment transactions on BackStack so then can be reversed
-//        override fun buttonClicked() {
-//        }
-    }
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        isLandscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE
-        loadFragmentBasedOnOrientation()
     }
 
     override fun buttonClicked() {
-        if (!isLandscape) {
-            supportFragmentManager.commit {
-                replace(R.id.container1, dieFragment)
-                addToBackStack(null)
-            }
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            val newFragment = DieFragment()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container1, newFragment)
+                .addToBackStack(null)
+                .commit()
         }
     }
-
-
-override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    isLandscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE
-    loadFragmentBasedOnOrientation()
-}
-
-override fun buttonClicked() {
-    if (!isLandscape) {
-        supportFragmentManager.commit {
-            replace(R.id.container1, dieFragment)
-            addToBackStack(null)
-        }
-    }
-}
 }
