552818bc47090c6c68bf8958b46d4de2f7a88a0a
KevinB6766 <tuu21653@temple.edu>
2025-03-26 15:46:23 -0400

    Temp Progress

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,10 +1,10 @@
 package edu.temple.dicethrow
 
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import kotlin.text.replace
 
 
 /*
@@ -19,6 +19,11 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+
+    private var isLandscape = false // Flag to track the orientation
+    private lateinit var buttonFragment: ButtonFragment // instantiate fragment
+    private lateinit var dieFragment: DieFragment // instantiate fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,16 +32,48 @@ class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
             - Show _only_ ButtonFragment if portrait
             - show _both_ fragments if Landscape
           */
-    }
+        buttonFragment = ButtonFragment()
+        dieFragment = DieFragment()
 
-    /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
+        isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
+        loadFragmentBasedOnOrientation()   // Check the orientation and load the appropriate fragment
+
+        /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
         */
 
-    // This callback function gets invoked when the child Fragment invokes it
-    // Remember to place Fragment transactions on BackStack so then can be reversed
-    override fun buttonClicked() {
+        // This callback function gets invoked when the child Fragment invokes it
+        // Remember to place Fragment transactions on BackStack so then can be reversed
+//        override fun buttonClicked() {
+//        }
+    }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        isLandscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE
+        loadFragmentBasedOnOrientation()
+    }
 
+    override fun buttonClicked() {
+        if (!isLandscape) {
+            supportFragmentManager.commit {
+                replace(R.id.container1, dieFragment)
+                addToBackStack(null)
+            }
+        }
     }
 
 
+override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    isLandscape = newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE
+    loadFragmentBasedOnOrientation()
+}
+
+override fun buttonClicked() {
+    if (!isLandscape) {
+        supportFragmentManager.commit {
+            replace(R.id.container1, dieFragment)
+            addToBackStack(null)
+        }
+    }
+}
 }
